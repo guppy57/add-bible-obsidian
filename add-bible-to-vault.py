7d73e83bbf8631a9048463a09c459dce8db6a3fc
Armaan Gupta <armaan@guppy.im>
2025-03-05 22:19:18 -0600

    holy crap it works

--- add-bible-to-vault.py
+++ add-bible-to-vault.py
@@ -3,6 +3,8 @@ import json
 import requests
 import pythonbible as bible
 from dotenv import load_dotenv
+import asyncio
+import aiohttp
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -10,6 +12,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 load_dotenv()
 driver = None
@@ -19,25 +23,25 @@ username = os.environ["USERNAME"]
 password = os.environ["PASSWORD"]
 
 bible_books = [
-    ("Genesis", 50, "GEN", "OT", 1, 16),
-    ("Exodus", 40, "EXO", "OT", 0, 0),
-    ("Leviticus", 27, "LEV", "OT", 0, 0),
-    ("Numbers", 36, "NUM", "OT", 0, 0),
-    ("Deuteronomy", 34, "DEU", "OT", 0, 0),
-    ("Joshua", 24, "JOS", "OT", 0, 0),
-    ("Judges", 21, "JDG", "OT", 0, 0),
-    ("Ruth", 4, "RUT", "OT", 0, 0),
-    ("1 Samuel", 31, "1SA", "OT", 0, 0),
-    ("2 Samuel", 24, "2SA", "OT", 0, 0),
-    ("1 Kings", 22, "1KI", "OT", 0, 0),
-    ("2 Kings", 25, "2KI", "OT", 0, 0),
-    ("1 Chronicles", 29, "1CH", "OT", 0, 0),
-    ("2 Chronicles", 36, "2CH", "OT", 0, 0),
-    ("Ezra", 10, "EZR", "OT", 0, 0),
-    ("Nehemiah", 13, "NEH", "OT", 0, 0),
-    ("Esther", 10, "EST", "OT", 0, 0),
-    ("Job", 42, "JOB", "OT", 0, 0),
-    ("Psalms", 150, "PSA", "OT", 0, 0),
+    ("Genesis", 50, "GEN", "OT", 1, 50),
+    ("Exodus", 40, "EXO", "OT", 1, 40),
+    ("Leviticus", 27, "LEV", "OT", 1, 27),
+    ("Numbers", 36, "NUM", "OT", 1, 36),
+    ("Deuteronomy", 34, "DEU", "OT", 1, 34),
+    ("Joshua", 24, "JOS", "OT", 1, 24),
+    ("Judges", 21, "JDG", "OT", 1, 21),
+    ("Ruth", 4, "RUT", "OT", 1, 4),
+    ("1 Samuel", 31, "1SA", "OT", 1, 31),
+    ("2 Samuel", 24, "2SA", "OT", 1, 24),
+    ("1 Kings", 22, "1KI", "OT", 1, 22),
+    ("2 Kings", 25, "2KI", "OT", 1, 25),
+    ("1 Chronicles", 29, "1CH", "OT", 1, 29),
+    ("2 Chronicles", 36, "2CH", "OT", 1, 36),
+    ("Ezra", 10, "EZR", "OT", 1, 10),
+    ("Nehemiah", 13, "NEH", "OT", 1, 13),
+    ("Esther", 10, "EST", "OT", 1, 10),
+    ("Job", 42, "JOB", "OT", 1, 42),
+    ("Psalms", 150, "PSA", "OT", 1, 144),
     ("Proverbs", 31, "PRO", "OT", 0, 0),
     ("Ecclesiastes", 12, "ECC", "OT", 0, 0),
     ("Song of Solomon", 8, "SNG", "OT", 0, 0),
@@ -111,104 +115,108 @@ key4 = os.environ["BIBLE_API_KEY_4"]
 key5 = os.environ["BIBLE_API_KEY_5"]
 key6 = os.environ["BIBLE_API_KEY_6"]
 api_keys = [key1, key2, key3, key4, key5, key6]
-current_api_key_num = 0
+current_api_key_num = 0 # change back to 0 for prod
 
 
 def retrieve_new_api_key():
-    global driver, current_api_key_num
+    global current_api_key_num
+
     print(f"Current AKN {current_api_key_num}")
-    current_api_key_num = current_api_key_num + 1
+    current_api_key_num += 1
     print(f"New AKN: {current_api_key_num}")
 
     if current_api_key_num > len(api_keys) - 1:
         print("Getting login page")
 
-        if driver is not None:
-            driver.quit()
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")  # Runs in the background (optional)
 
-        driver = webdriver.Chrome()
+        with webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options) as driver:
+            driver.get("https://scripture.api.bible/login")
 
-        driver.get("https://scripture.api.bible/login")
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.NAME, "username"))
+            ).send_keys(username)
+            driver.find_element(By.NAME, "password").send_keys(password)
+            print("Hitting submit button")
+            driver.find_element(By.NAME, "password").send_keys(Keys.RETURN)
+            WebDriverWait(driver, 10).until(EC.url_changes("https://scripture.api.bible"))
 
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.NAME, "username"))
-        ).send_keys(username)
-        driver.find_element(By.NAME, "password").send_keys(password)
-        print("Hitting submit button")
-        driver.find_element(By.NAME, "password").send_keys(Keys.RETURN)
-        WebDriverWait(driver, 10).until(EC.url_changes("https://scripture.api.bible"))
+            print("Opening new app page")
+            driver.get("https://scripture.api.bible/admin/applications/new")
 
-        print("Opening new app page")
-        driver.get("https://scripture.api.bible/admin/applications/new")
+            application_name = f"bible-to-markdown-{current_api_key_num}"
+            description = "Placing bible in markdown on my desktop"
+            commercial = "Non-commercial"
 
-        application_name = f"bible-to-markdown-{current_api_key_num}"
-        description = "Placing bible in markdown on my desktop"
-        commercial = "Non-commercial"
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.NAME, "application[name]"))
+            ).send_keys(application_name)
+            driver.find_element(By.NAME, "application[description]").send_keys(description)
 
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.NAME, "application[name]"))
-        ).send_keys(application_name)
-        driver.find_element(By.NAME, "application[description]").send_keys(description)
+            dropdown = Select(driver.find_element(By.ID, "application_commercial_id"))
+            dropdown.select_by_visible_text(commercial)
 
-        dropdown = Select(driver.find_element(By.ID, "application_commercial_id"))
-        dropdown.select_by_value(commercial)
+            print("Submitting form")
+            driver.find_element(By.NAME, "commit").click()
 
-        print("Submitting form")
-        driver.find_element(By.NAME, "commit").click()
+            WebDriverWait(driver, 12).until(
+                EC.presence_of_element_located((By.ID, "user-key"))
+            )
 
-        WebDriverWait(driver, 12).until(
-            EC.presence_of_element_located((By.ID, "user-key"))
-        )
+            soup = BeautifulSoup(driver.page_source, "html.parser")
+            api_key_element = soup.find("code", {"id": "user-key"})
+            new_api_key = None
 
-        soup = BeautifulSoup(driver.page_source, "html.parser")
-        api_key_element = soup.find("code", {"id": "user-key"})
-        new_api_key = None
+            if api_key_element:
+                new_api_key = api_key_element.text.strip()
+                print("Extracted API Key:", new_api_key)
+            else:
+                print("API Key not found")
 
-        if api_key_element:
-            new_api_key = api_key_element.text.strip()
-            print("Extracted API Key:", new_api_key)
-        else:
-            print("API Key not found")
-
-        driver.quit()
-        driver = None
-        api_keys.append(new_api_key)
-
-    return None
+            api_keys.append(new_api_key)
 
 
-def make_request(path):
+async def make_request(path):
     url = BIBLE_API_URL + path
     api_key = api_keys[current_api_key_num]
 
-    try:
-        response = requests.get(url, headers={"api-key": api_key})
-
-        if response.status_code == 200:
-            print("Found verse")
-        elif response.status_code == 404:
-            print("Verse missing")
-        else:
-            print(f"Some exception: {response.text}")
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.get(url, headers={"api-key": api_key}) as response:
+                if response.status == 200:
+                    print("Found verse")
+                    return await response.json()
+                elif response.status == 404:
+                    print("Verse missing")
+                    return None
+                else:
+                    print(f"Some exception: {await response.text()}")
+                    retrieve_new_api_key()
+                    return await make_request(path)
+        except Exception as e:
+            print(f"KEY HIT EXCEPTION: {e}")
             retrieve_new_api_key()
-            return make_request(path)
-
-        return response
-    except Exception as e:
-        # assumption is that we hit a rate limit
-        print(f"KEY_1 HIT EXCEPTION: {e}")
-        retrieve_new_api_key()
-        return make_request(path)
+            return await make_request(path)
 
 
-def get_verse_text(bible, verse_id):
+async def get_verse_text(bible, verse_id):
     params = "content-type=text&include-notes=false&include-chapter-numbers=false&include-verse-numbers=true&include-verse-spans=false"
     path = f"/{bible}/verses/{verse_id}?{params}"
-    resp = make_request(path)
+    resp = await make_request(path)
 
-    data = json.loads(resp.text)
-    print(data)
-    return data["data"]["content"]
+    if resp and "data" in resp:
+        return resp["data"]["content"]
+    return ""
+
+
+async def fetch_all_verses(verse_id, hebrew_id, greek_id):
+    tasks = [
+        get_verse_text(BSB_BIBLE_ID, verse_id),
+        get_verse_text(greek_id, verse_id),
+        get_verse_text(hebrew_id, verse_id)
+    ]
+    return await asyncio.gather(*tasks)
 
 
 def get_formatted_verse(text):
@@ -244,12 +252,8 @@ def import_bible_into_existing_bible_files(directory):
 
             filename = f"{book} {chapter}.md"
             file_path = os.path.join(f"{directory}/{folder}", filename)
+            open("filename", "w").close()  # Deletes all existing content
 
-            print(f"Writing to: {filename}\nPath: {file_path}")
-
-            open("filename", "w").close()  # deletes all existing content
-
-            # get number of verses using api
             num_of_verses = bible.get_number_of_verses(book_val, chapter)
             print(f"Number of verses: {num_of_verses}")
 
@@ -257,30 +261,21 @@ def import_bible_into_existing_bible_files(directory):
             greek_scripture = f"##### {greek_name}\n\n"
             hebrew_scripture = f"##### {hebrew_name}\n\n"
 
-            for i in range(1, num_of_verses + 1):
-                verse_id = f"{book_id}.{chapter}.{i}"
-                print(verse_id)
+            async def fetch_chapter():
+                nonlocal english_scripture, greek_scripture, hebrew_scripture
+                for i in range(1, num_of_verses + 1):
+                    verse_id = f"{book_id}.{chapter}.{i}"
+                    print(verse_id)
 
-                try:
-                    english = get_verse_text(BSB_BIBLE_ID, verse_id)
-                    print(f"English: {english}")
-                    english_scripture += get_formatted_verse(english)
-                except Exception as e:
-                    print(e)
+                    try:
+                        english, greek, hebrew = await fetch_all_verses(verse_id, hebrew_id, greek_id)
+                        english_scripture += get_formatted_verse(english)
+                        greek_scripture += get_formatted_verse(greek)
+                        hebrew_scripture += get_formatted_verse(hebrew)
+                    except Exception as e:
+                        print(e)
 
-                try:
-                    greek = get_verse_text(greek_id, verse_id)
-                    print(f"Greek: {greek}")
-                    greek_scripture += get_formatted_verse(greek)
-                except Exception as e:
-                    print(e)
-
-                try:
-                    hebrew = get_verse_text(hebrew_id, verse_id)
-                    print(f"Hebrew: {hebrew}")
-                    hebrew_scripture += get_formatted_verse(hebrew)
-                except Exception as e:
-                    print(e)
+            asyncio.run(fetch_chapter())
 
             with open(file_path, "w") as file:
                 file.write(
@@ -291,9 +286,9 @@ def import_bible_into_existing_bible_files(directory):
 
 
 import_bible_into_existing_bible_files(DIRECTORY)
-# retrieve_new_api_key()
+retrieve_new_api_key()
 
-# verses_to_test = [1, 2, 3, 4, 5, 6]
-# for i in verses_to_test:
-#     verse_text = get_verse_text(BSB_BIBLE_ID, f"GEN.1.{i}")
-#     print(verse_text)
+#verses_to_test = [1, 2, 3, 4, 5, 6]
+#for i in verses_to_test:
+#    verse_text = get_verse_text(BSB_BIBLE_ID, f"GEN.1.{i}")
+#    print(verse_text)
